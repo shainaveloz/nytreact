9c3595f06f18eb141fe7f7d41dc838bb98b07a5b
shainaveloz <shaina.veloz@gmail.com>
2016-08-08 00:15:48 -0400

    added react syntax to search

--- app/components/children/search.js
+++ app/components/children/search.js
@@ -1,23 +1,34 @@
 var React = require('react');
 
-<div class="row">
-    <form class="col s12">
-        <div class="row">
-            <div class="input-field col s6">
-                <input placeholder="Placeholder" id="topic" type="text" class="validate">
-                    <label for="topic">Topic</label>
+// utlizing the axios library to perform GET/POST requests
+var axios = require('axios');
+
+var search = React.createClass({
+
+    render: function(){
+        return (
+            <div className="row">
+                <form className="col s12">
+                    <div className="row">
+                        <div className="input-field col s6">
+                            <input placeholder="Placeholder" id="topic" type="text" className="validate">
+                                <label for="topic">Topic</label>
+                        </div>
+                        <div className="input-field col s6">
+                            <input id="start_year" type="text" className="validate">
+                                <label for="start_year">Start Year</label>
+                        </div>
+                        <div className="input-field col s6">
+                            <input id="end_year" type="text" className="validate">
+                                <label for="end_year">Start Year</label>
+                        </div>
+                    </div>
+                    <button className="btn waves-effect waves-light" type="submit" name="action" onClick={this.handleClick}>Submit
+                        <i className="material-icons right">send</i>
+                    </button>
+                </form>
             </div>
-            <div class="input-field col s6">
-                <input id="start_year" type="text" class="validate">
-                    <label for="start_year">Start Year</label>
-            </div>
-            <div class="input-field col s6">
-                <input id="end_year" type="text" class="validate">
-                    <label for="end_year">Start Year</label>
-            </div>
-        </div>
-        <button class="btn waves-effect waves-light" type="submit" name="action">Submit
-            <i class="material-icons right">send</i>
-        </button>
-    </form>
-</div>
+        )
+    }
+})
+
